c6f2b9a676a56783c899cc3d465539652f928019
Mari Calmer <superlassig@gmail.com>
2021-06-14 12:41:10 +0200

    add cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,26 @@ class User < ApplicationRecord
 
   validates :slug, uniqueness: true, case_sensitive: false
   validates :username, uniqueness: true, case_sensitive: false, presence: true
+
+  after_create :update_slug
+  before_update :assign_slug
+
+  def create_slug
+    slug = self.username.parameterize
+    User.where.not(id: self.id).find_by(slug: slug).nil? ? slug : slug + slug.length.to_s
+  end
+
+  def update_slug
+    update slug: assign_slug
+  end
+
+  def to_param
+    slug
+  end
+
+  private
+
+  def assign_slug
+    self.slug = create_slug
+  end
 end
