0e66ee6b8353b51fe5dc07496d9d2478933aaf80
Mari Calmer <superlassig@gmail.com>
2022-03-16 20:38:05 +0100

    edit job form

--- app/javascript/components/switch_payment_rate.js
+++ app/javascript/components/switch_payment_rate.js
@@ -1,9 +1,8 @@
 const setDropdown = () => {
   const dropdown = document.querySelector(".dropdown-payment-js");
   dropdown.addEventListener("change", function() {
-    console.log("callback")
     const text = document.querySelector(".job-payment-js");
-    if (dropdown.options[dropdown.selectedIndex].value === "hourly rate") {
+    if (dropdown.options[dropdown.selectedIndex].value === "hourly_rate") {
       text.innerHTML = "€ per hour"
     } else {
       text.innerHTML = "€ total"
