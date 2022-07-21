7acc286b573e2bb9e52a0ebf4000e44eec7286b4
Mari Calmer <superlassig@gmail.com>
2022-07-21 18:43:41 +0200

    homepage mobile with tap btn

--- app/javascript/components/homepage_slides.js
+++ app/javascript/components/homepage_slides.js
@@ -1,87 +1,48 @@
-// const TIME_OUT = 2000 // same transition time of the sections
-// const body = document.querySelector('body')
-// const sectionsQty = document.querySelectorAll('section').length
-
-// let startFlag = true
-// let initialScroll = window.scrollY
 let qty = 1, section = null, direction = "down"
 
-// const changeNavColor = () => {
-//   const navElements = document.querySelectorAll(".nav-homepage-js")
-//   console.log(navElements)
-//   navElements.forEach((element)=>{
-//     if (qty === 2 && element.localName === "span") {
-//       element.style.background = "#858585"
-//     } else if ((qty === 2 && element.localName === "a") || (qty === 2 && element.localName === "li") || (qty === 2 && element.localName === "div")) {
-//       element.style.color = "#858585"
-//     } else if (element.localName === "span") {
-//       element.style.background = "#858585"
-//     } else if ((element.localName === "a") || (element.localName === "li") || (element.localName === "div")) {
-//       element.style.color = "#858585"
-//     }
-//   })
-// }
+
+const hideTapMobileBtn = () => {
+  const btn = document.querySelector(".tap-your-screen-btn")
+  if (btn) {
+    btn.classList.toggle("hide-btn")
+  }
+}
 
 const moveSections = () => {
-  // if (startFlag) {
-    // const scrollDown = window.scrollY >= initialScroll + 0.5
-    // const clickLimit = qty >= 1 && qty <= sectionsQty
-    const openModal = document.querySelectorAll(".modal.show").length > 0
-    // console.log(clickLimit)
-    // if (clickLimit) {
-      // body.style.overflowY = 'hidden' // Lock el scroll
-
-      if (direction == "down" && !openModal) {
-
-
-        if (qty === 1) {
-          section = document.querySelector(`.s${qty + 1}`)
-          section.style.transform = 'translateX(-100vw)'
-          qty++
-        } else if (qty === 2) {
-          section = document.querySelector(`.s${qty + 1}`)
-          section.style.transform = 'translateY(-200vh)'
-          qty++
-        } else if (qty === 3) {
-          section = document.querySelector(`.s${qty}`)
-          section.style.transform = 'translateY(200vh)'
-          direction = "up"
-          qty--
-        }
-
-        // changeNavColor()
-
-      } else if (direction == "up" && !openModal) {
-
-
-        if (qty === 1) {
-          section = document.querySelector(`.s${qty + 1}`)
-          section.style.transform = 'translateX(-100vw)'
-          direction = "down"
-          qty++
-        } else if (qty === 2) {
-          section = document.querySelector(`.s${qty}`)
-          section.style.transform = 'translateX(100vw)'
-          qty--
-        }
-
-        // changeNavColor()
-      }
-    // }
-    // console.log(startFlag)
-    console.log('SLIDE', qty)
-    console.log(direction)
-    // console.log(window.scrollY)
-
-    // setTimeout(() => {
-    //   initialScroll = window.scrollY
-    //   // startFlag = true
-    //   body.style.overflowY = 'scroll'
-    // }, TIME_OUT)
-
-    // startFlag = false
-  // }
-
+  const openModal = document.querySelectorAll(".modal.show").length > 0
+  if (direction == "down" && !openModal) {
+    if (qty === 1) {
+      section = document.querySelector(`.s${qty + 1}`)
+      section.style.transform = 'translateX(-100vw)'
+      qty++
+    } else if (qty === 2) {
+      section = document.querySelector(`.s${qty + 1}`)
+      section.style.transform = 'translateY(-200vh)'
+      qty++
+      setTimeout(() => {
+        hideTapMobileBtn()
+      }, 400)
+    } else if (qty === 3) {
+      section = document.querySelector(`.s${qty}`)
+      section.style.transform = 'translateY(200vh)'
+      direction = "up"
+      qty--
+      setTimeout(() => {
+        hideTapMobileBtn()
+      }, 200)
+    }
+  } else if (direction == "up" && !openModal) {
+    if (qty === 1) {
+      section = document.querySelector(`.s${qty + 1}`)
+      section.style.transform = 'translateX(-100vw)'
+      direction = "down"
+      qty++
+    } else if (qty === 2) {
+      section = document.querySelector(`.s${qty}`)
+      section.style.transform = 'translateX(100vw)'
+      qty--
+    }
+  }
 }
 
 const homepageSlides = () => {
@@ -96,7 +57,6 @@ const homepageSlides = () => {
 }
 
 const delayHomepageSlides = () => {
-  // setTimeout(homepageSlides, 1200);
   homepageSlides()
 }
 
