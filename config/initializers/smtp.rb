0ae9dfb92fa4a5891c8dedacb62127c371d227e6
Mari Calmer <superlassig@gmail.com>
2022-07-26 17:41:39 +0200

    update smtp.rb

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   address: ENV['MAILGUN_SMTP_SERVER'],
   port: ENV['MAILGUN_SMTP_PORT'],
-  domain: 'www.infamousberlin.com',
+  domain: 'infamousberlin.com',
   user_name: ENV['MAILGUN_SMTP_LOGIN'],
   password: ENV['MAILGUN_SMTP_PASSWORD'],
   authentication: :plain,
