88b45d3d94516efdf2b7386f2f7ac6aa303a9f7b
Mari Calmer <superlassig@gmail.com>
2022-07-26 13:05:11 +0200

    click event on slides after going back to homepage

--- app/javascript/components/homepage_slides.js
+++ app/javascript/components/homepage_slides.js
@@ -1,61 +1,68 @@
-let qty = 1, section = null, direction = "down"
+let nextSection = null, section = null, sections = null, viewportSection = null
 
-
+// const isInViewport = (element) => {
+//   const rect = element.getBoundingClientRect();
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// }
 const hideTapMobileBtn = () => {
   const btn = document.querySelector(".tap-your-screen-btn")
   if (btn) {
     btn.classList.toggle("hide-btn")
   }
 }
-
 const moveSections = () => {
   const openModal = document.querySelectorAll(".modal.show").length > 0
-  if (direction == "down" && !openModal) {
-    if (qty === 1) {
-      section = document.querySelector(`.s${qty + 1}`)
-      section.style.transform = 'translateX(-100vw)'
-      qty++
-    } else if (qty === 2) {
-      section = document.querySelector(`.s${qty + 1}`)
-      section.style.transform = 'translateY(-200vh)'
-      qty++
-      setTimeout(() => {
-        hideTapMobileBtn()
-      }, 400)
-    } else if (qty === 3) {
-      section = document.querySelector(`.s${qty}`)
-      section.style.transform = 'translateY(200vh)'
-      direction = "up"
-      qty--
+  sections = document.querySelectorAll('[class*="layer"]')
+  viewportSection = Array.from(sections).filter( section => section.classList.contains("section-in-viewport-js") == true )
+  console.log("sections")
+  console.log(sections)
+  console.log("viewportSection")
+  console.log(viewportSection)
+  if (!openModal) {
+    if (viewportSection.length === 1) {
+      nextSection = document.querySelector(".s2")
+      nextSection.style.transform = 'translateX(-100vw)'
+      nextSection.classList.remove("backwards")
+      nextSection.classList.add("section-in-viewport-js")
+    } else if (viewportSection.length === 2) {
+      section = document.querySelector(".s2")
+      if (section.classList.contains("backwards")) {
+        section.style.transform = 'translateX(100vw)'
+        section.classList.remove("section-in-viewport-js")
+      } else {
+        nextSection = document.querySelector(".s3")
+        nextSection.style.transform = 'translateY(-200vh)'
+        nextSection.classList.add("section-in-viewport-js")
+        section.classList.add("backwards")
+        setTimeout(() => {
+          hideTapMobileBtn()
+        }, 400)
+      }
+    } else if (viewportSection.length === 3) {
+      nextSection = document.querySelector(".s3")
+      nextSection.style.transform = 'translateY(200vh)'
+      nextSection.classList.remove("section-in-viewport-js")
       setTimeout(() => {
         hideTapMobileBtn()
       }, 200)
     }
-  } else if (direction == "up" && !openModal) {
-    if (qty === 1) {
-      section = document.querySelector(`.s${qty + 1}`)
-      section.style.transform = 'translateX(-100vw)'
-      direction = "down"
-      qty++
-    } else if (qty === 2) {
-      section = document.querySelector(`.s${qty}`)
-      section.style.transform = 'translateX(100vw)'
-      qty--
-    }
   }
 }
-
 const homepageSlides = () => {
   const homepage = document.querySelector(".homepage");
   homepage.addEventListener("click", moveSections)
   const exits = document.querySelectorAll(".scroll-event-exit-js")
   exits.forEach((exit) => {
     exit.addEventListener("click", function() {
-      window.removeEventListener('click', moveSections);
+      // window.removeEventListener('click', moveSections);
     })
   })
 }
-
 const delayHomepageSlides = () => {
   homepageSlides()
 }
