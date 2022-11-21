722a413915bad7158ba6fa1dfba887462f7a5c90
Mari Calmer <superlassig@gmail.com>
2022-11-21 19:24:04 +0100

    add background-image to audio preview in project new/update

--- app/javascript/components/file_preview.js
+++ app/javascript/components/file_preview.js
@@ -1,5 +1,5 @@
 const updateFilesList = (index) => {
-  let initialFiles = document.getElementById("project_attachments")
+  let initialFiles = document.querySelector(".project-attachments-upload-btn-js")
   const initialArray = Array.from(initialFiles.files)
   initialArray.splice(index, 1)
   let newList = new DataTransfer();
@@ -26,21 +26,20 @@ const setupDeleteBtn = () => {
 }
 const displayDeleteBtn = (img, placeholder) => {
   const deleteWrapper = document.createElement("div")
-  deleteWrapper.classList.add("wrapper-preview-delete", "delete-previewed-js")
+  deleteWrapper.classList.add("form-page__preview-delete-wrapper", "delete-previewed-js")
   const deleteBtn = document.createElement("div")
   const deleteBtnContent = document.createTextNode("✖")
   deleteBtn.appendChild(deleteBtnContent)
-  deleteBtn.classList.add("project-photo-preview-delete")
+  deleteBtn.classList.add("form-page__preview-delete-btn")
   const previewCard = document.createElement("div")
-  previewCard.classList.add("project-photo-preview-card", "preview-img-js")
+  previewCard.classList.add("form-page__preview-photo-wrapper", "preview-img-js")
   deleteWrapper.appendChild(deleteBtn)
   previewCard.appendChild(img)
   previewCard.appendChild(deleteWrapper)
   placeholder.append(previewCard)
 }
 const catchFilePathUserForm = (userAvatarUploadBtn) => {
-  const fileInput = userAvatarUploadBtn.querySelector("#user_photo")
-  console.log(fileInput)
+  const fileInput = userAvatarUploadBtn.querySelector(".user-photo-js")
   if (fileInput.files.length > 0) {
     const imgUri = fileInput.files[0]
     const placeholder = document.querySelector(".placeholder-preview-js");
@@ -57,21 +56,24 @@ const catchFilePathProjectForm = (projectAttachmentsUploadBtn) => {
     if (projectAttachmentsUploadBtn.files[i].type === "video/mp4") {
       const file = document.createElement("video");
       const imgUri = projectAttachmentsUploadBtn.files[i];
-      file.classList.add("project-photo-preview");
+      file.classList.add("form-page__preview-photo");
       file.src = URL.createObjectURL(imgUri);
       file.setAttribute("index", i);
+      file.setAttribute("controls", "controls");
+      file.setAttribute("muted", "muted");
       displayDeleteBtn(file, placeholder);
     } else if (projectAttachmentsUploadBtn.files[i].type === "audio/mpeg") {
-      const file = document.createElement("img");
+      const file = document.createElement("video");
       const imgUri = projectAttachmentsUploadBtn.files[i];
-      file.classList.add("project-photo-preview");
-      file.src = "/assets/logo-audio.png";
+      file.classList.add("form-page__preview-photo", "form-page__preview-photo--audio-poster");
+      file.src = URL.createObjectURL(imgUri);
       file.setAttribute("index", i);
+      file.setAttribute("controls", "controls");
       displayDeleteBtn(file, placeholder);
     } else {
       const file = document.createElement("img");
       const imgUri = projectAttachmentsUploadBtn.files[i];
-      file.classList.add("project-photo-preview");
+      file.classList.add("form-page__preview-photo");
       file.src = URL.createObjectURL(imgUri);
       file.setAttribute("index", i);
       displayDeleteBtn(file, placeholder);
