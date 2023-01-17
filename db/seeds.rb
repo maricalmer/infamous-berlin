e730e98882ddde6ecc3785516fd400df29eb332f
Mari Calmer <superlassig@gmail.com>
2023-01-17 13:57:10 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,81 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# SPLASH
-# >>> sculpture
-# https://unsplash.com/@lauraadaiphoto
-# https://unsplash.com/photos/PcjuUQN1mFc
-# https://unsplash.com/photos/oSVJYgL0qUs
-# https://unsplash.com/photos/4QTRb16OOts
-# >>> modern art
-# https://unsplash.com/photos/_8zO0LaCxRQ
-# https://unsplash.com/photos/h2uCymK1oYc
-# https://unsplash.com/photos/ZZTbCMOWAGk
-# >>> profile pic
-# https://unsplash.com/photos/3TLl_97HNJo
-
-# PEXELS
-# >>> login (P.Berg)
-# https://www.pexels.com/video/a-man-lovingly-touching-the-face-of-a-head-bust-sculpture-3712948/
-# https://www.pexels.com/video/painting-one-s-face-with-black-4788417/
-# https://www.pexels.com/video/fashion-woman-art-model-4962903/
-# https://www.pexels.com/video/a-man-playing-the-piano-while-a-woman-playing-the-guitar-while-singing-8039634/
-# https://www.pexels.com/video/a-rapper-recording-in-a-studio-8132052/
-
 
 require "open-uri"
 
 puts "cleaning up DB"
-puts "messages..."
 Message.destroy_all if Rails.env.development?
-puts "chatrooms..."
 Chatroom.destroy_all if Rails.env.development?
-puts "inquiries..."
 Inquiry.destroy_all if Rails.env.development?
 ActiveRecord::Base.connection.reset_pk_sequence!('inquiries')
-puts "jobs..."
 Job.destroy_all if Rails.env.development?
 ActiveRecord::Base.connection.reset_pk_sequence!('jobs')
-puts "collabs..."
 Collab.destroy_all if Rails.env.development?
 ActiveRecord::Base.connection.reset_pk_sequence!('collabs')
-puts "projects..."
 Project.destroy_all if Rails.env.development?
 ActiveRecord::Base.connection.reset_pk_sequence!('projects')
-puts "users..."
 User.destroy_all if Rails.env.development?
 ActiveRecord::Base.connection.reset_pk_sequence!('users')
 puts "DB is empty"
 puts "-----------"
 puts "creating users"
-20.times do
+5.times do
   user = User.create!(
     email: Faker::Internet.email,
     password: "123456",
     username: Faker::Internet.unique.username,
     skills: Faker::Job.key_skill.downcase,
     bio: Faker::Lorem.sentence(word_count: rand(5..50)),
-    title: Faker::Lorem.sentence(word_count: rand(1..6))
+    title: Faker::Lorem.sentence(word_count: rand(1..6)),
+    confirmed_at: Time.now.utc
   )
-  user.update(website: "thewebsite.com") if rand(1..2) == 1
-  user.update(facebook: "facebook-account-url.com") if rand(1..2) == 1
-  user.update(instagram: "instagram-account-url.com") if rand(1..2) == 1
-  user.update(soundcloud: "soundcloud-account-url.com") if rand(1..2) == 1
-  user.update(youtube: "youtube-account-url.com") if rand(1..2) == 1
-  user.update(mixcloud: "mixcloud-account-url.com") if rand(1..2) == 1
-  user.update(linkedin: "linkedin-account-url.com") if rand(1..2) == 1
-  user.update(twitter: "twitter-account-url.com") if rand(1..2) == 1
-  user.photo.attach(
-    io: URI.open('https://res.cloudinary.com/dbpv82leg/image/upload/v1624021222/photo-1522075469751-3a6694fb2f61.jpg'),
-    filename: Faker::File.file_name(ext: 'jpg'),
-    content_type: 'image/jpg'
-    )
   puts "user #{user.id} is created"
 end
-puts "20 fresh new users"
+puts "5 new users"
 puts "creating projects"
-20.times do
-  project = Project.create!(
+2.times do
+  project = Project.new(
     title: Faker::Book.unique.title,
     description: Faker::Quote.matz,
     category: ['painting', 'print', 'photography', 'sculpture', 'furniture', 'fashion', 'other'].sample,
@@ -89,85 +49,42 @@ puts "creating projects"
     user: User.all.sample
   )
   project.attachments.attach(
-    io: URI.open('https://res.cloudinary.com/dbpv82leg/image/upload/v1600074118/w54i1yub1kgz5rvd22i0c5wr4pwm.jpg'),
+    io: URI.open('https://images.unsplash.com/photo-1541961017774-22349e4a1262?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1058&q=80'),
     filename: Faker::File.file_name(ext: 'jpg'),
     content_type: 'image/jpg'
   )
-  project.attachments.attach(
-    io: URI.open('https://res.cloudinary.com/dbpv82leg/image/upload/v1598882767/wfauj0r0q92zxqxs6lpp.jpg'),
-    filename: Faker::File.file_name(ext: 'jpg'),
-    content_type: 'image/jpg'
-  )
-  project.attachments.attach(
-    io: URI.open('https://res.cloudinary.com/dbpv82leg/image/upload/v1598882740/rbhbfgjs12fnd3krfy0i.jpg'),
-    filename: Faker::File.file_name(ext: 'jpg'),
-    content_type: 'image/jpg'
-  )
+  project.save!
   puts "project #{project.id} is created"
 end
-puts "20 fresh new projects"
-puts "updating upcoming projects"
-upcoming_projects = Project.find([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
-upcoming_projects.each do |project|
-  project.update(status: "upcoming")
-end
-puts "10 upcoming projects updated"
-puts "creating 1 message"
-chatroom = Chatroom.create!(
-  author: User.first,
-  receiver: Project.first.user
+puts "2 new projects"
+puts "updating upcoming project"
+upcoming_project = Project.find(1)
+upcoming_project.update(status: "upcoming")
+puts "1 project updated to upcoming"
+puts "creating 1 job"
+project = Project.first
+job = Job.create!(
+  title: ["video editor", "photographer", "model", "stylist", "designer", "dancer", "musician"].sample,
+  skills_needed: ["acting", "dancing", "photoshop"].sample,
+  description: Faker::Lorem.sentence(word_count: 5, random_words_to_add: 10),
+  location: ["Mitte", "P.Berg", "Wedding", "Kreuzberg", "Neukölln", "Friedrichshain", "remote"].sample,
+  payment: ["fixed_rate", "hourly_rate"].sample,
+  money: [100, 150, 200, 250, 300, 350].sample,
+  status: ["open", "close"].sample,
+  project_id: project.id
 )
-message = Message.create!(
-  content: "Test message from #{Time.now} - [seeds]",
-  anchor_id: 1,
-  read_by_receiver: false,
-  chatroom: chatroom,
-  user: User.first
+puts "job #{job.id} is created"
+puts "1 new job"
+puts "creating inquiry"
+job = Job.first
+user = User.last
+inquiry = Inquiry.create!(
+  experience: Faker::Lorem.sentence(word_count: 10, random_words_to_add: 10),
+  motivation: Faker::Lorem.sentence(word_count: 10, random_words_to_add: 10),
+  job_id: job.id,
+  user_id: user.id
 )
-puts "creating collabs"
-projects = Project.first(15)
-users = User.first(15)
-10.times do
-  members = users - [projects.first.user]
-  collab = Collab.create!(
-    project: projects.first,
-    member: members.sample
-  )
-  projects.delete(projects.first)
-  puts "collab #{collab.id} is created"
-end
-puts "10 fresh new collabs"
-puts "creating jobs"
-projects = Project.first(10)
-10.times do
-  job = Job.create!(
-    title: ["video editor", "photographer", "model", "stylist", "designer", "dancer", "musician"].sample,
-    skills_needed: ["acting", "dancing", "photoshop"].sample,
-    description: Faker::Lorem.sentence(word_count: 5, random_words_to_add: 10),
-    location: ["Mitte", "P.Berg", "Wedding", "Kreuzberg", "Neukölln", "Friedrichshain", "remote"].sample,
-    payment: ["fixed_rate", "hourly_rate"].sample,
-    money: [100, 150, 200, 250, 300, 350].sample,
-    status: ["open", "close"].sample,
-    project_id: projects.first.id
-  )
-  projects.delete(projects.first)
-  puts "job #{job.id} is created"
-end
-puts "10 fresh new jobs"
-puts "creating inquiries"
-jobs = Job.first(10)
-users = User.first(10)
-10.times do
-  inquiry = Inquiry.create!(
-    experience: Faker::Lorem.sentence(word_count: 10, random_words_to_add: 10),
-    motivation: Faker::Lorem.sentence(word_count: 10, random_words_to_add: 10),
-    job_id: jobs.first.id,
-    user_id: users.first.id == jobs.first.project.user.id ? users.last.id : users.first.id
-  )
-  jobs.delete(inquiry.job)
-  users.delete(inquiry.user)
-  puts "inquiry #{inquiry.id} is created"
-end
-puts "10 fresh new inquiries"
+puts "inquiry #{inquiry.id} is created"
+puts "1 new inquiry"
 puts "-----------"
 puts "SEEDING IS DONE!!!"
