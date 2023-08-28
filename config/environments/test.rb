5a940a415c0ef9cddb80f04721f2516fb2f2ceb7
Mari Calmer <superlassig@gmail.com>
2023-08-28 16:49:04 +0200

    feat: comments cleanup with focus on devise controllers + gemfile for selenium webdrivers

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,5 +46,4 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-  # Webdrivers::Chromedriver.required_version = "114.0.5735.90"
 end
