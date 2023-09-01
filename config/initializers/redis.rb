e0a6af4a4461a5e820fda1b65faf00b625109c07
Mari Calmer <superlassig@gmail.com>
2023-09-01 21:18:08 +0200

    fix: remove redis support gems + initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-Redis.current = Redis.new(url: ENV["REDISCLOUD_URL"], port: ENV['REDIS_PORT'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+# $redis = Redis.new(url: ENV["REDISCLOUD_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
