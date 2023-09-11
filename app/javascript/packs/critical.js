119020a2d810a08f76e3b25d7c6bd8276bd36d94
Mari Calmer <superlassig@gmail.com>
2023-09-11 13:52:23 +0200

    feat: create critical js file and download it first

---
+++ app/javascript/packs/critical.js
@@ -0,0 +1,15 @@
+import { initCursor } from '../components/homepage_cursor';
+import { firstVisitCheck } from '../components/homepage_preloader';
+import { homepageSlides } from '../components/homepage_slides';
+
+document.addEventListener('turbolinks:load', () => {
+  if (document.querySelectorAll(".homepage-slides-js").length) {
+    homepageSlides();
+  }
+  if (document.querySelectorAll(".preloader-js").length) {
+    firstVisitCheck();
+  }
+  if (document.querySelectorAll(".homepage-cursor-js").length) {
+    initCursor();
+  }
+});
