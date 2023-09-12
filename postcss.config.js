ef31a7fe6fc32663d0b8d7036f58c475a8ef862f
Mari Calmer <superlassig@gmail.com>
2023-09-12 19:44:53 +0200

    fix: use fullhuman/postcss-purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-module.exports = {
+let environment = {
   plugins: [
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -7,6 +8,17 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
   ]
 }
+
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./app/**/*.html.erb', './app/**/*.js', './app/helpers/**/*.rb'],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+module.exports = environment
