3d4cf2f828e80a95e5ed8a2887641354cb399b72
Mari Calmer <superlassig@gmail.com>
2024-06-30 20:29:44 +0200

    feat: add recaptcha

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'cloudinary'
 # Use Active Storage variant
 gem 'active_storage_validations', '~> 0.9.6'
 # gem 'image_processing', '~> 1.2'
+gem "recaptcha"
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
+# class Users::RegistrationsController < Devise::RegistrationsController
+#   # The path used after sign up for inactive accounts.
+#   def after_inactive_sign_up_path_for(_resource)
+#     confirmation_pending_path
+#   end
+# end
+
 class Users::RegistrationsController < Devise::RegistrationsController
-  # The path used after sign up for inactive accounts.
+  def create
+    if verify_recaptcha
+      super
+    else
+      build_resource(sign_up_params)
+      clean_up_passwords(resource)
+      flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
+      flash.delete :recaptcha_error
+      render :new
+    end
+  end
   def after_inactive_sign_up_path_for(_resource)
     confirmation_pending_path
   end
