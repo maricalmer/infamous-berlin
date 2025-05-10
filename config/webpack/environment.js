aeabca06694e286b32d39e342a4ba8fa147d0689
Mari Calmer <superlassig@gmail.com>
2025-05-10 23:13:32 +0200

    fix: fontawesome bug from rails_admin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,18 +1,21 @@
 const { environment } = require('@rails/webpacker')
-
-// const webpack = require('webpack');
-// // Preventing Babel from transpiling NodeModules packages
-// environment.loaders.delete('nodeModules');
-// module.exports = environment
-
-
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
+
+environment.plugins.prepend('Provide', 
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     Popper: ['popper.js', 'default']
   })
 )
+
+environment.config.merge({
+  resolve: {
+    alias: {
+      'jquery-ui/ui': 'jquery-ui/ui'
+    }
+  }
+})
+
 environment.loaders.delete('nodeModules');
-module.exports = environment
+module.exports = environment;
