ccb72417c27e624374a17da033d4990f78825279
Mari Calmer <superlassig@gmail.com>
2025-05-11 00:08:14 +0200

    fix: update smtp credits

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-  address: ENV['MAILJET_SMTP_SERVER'],
-  port: ENV['MAILJET_PORT'],
-  domain: 'infamousberlin.com',
+  address: "in-v3.mailjet.com",
+  port: 587,
+  domain: 'herokuapp.com',
   user_name: ENV['MAILJET_API_KEY'],
   password: ENV['MAILJET_SECRET_KEY'],
   enable_starttls_auto: true,
